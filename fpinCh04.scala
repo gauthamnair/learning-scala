737898bb0c882675b3aacb87f315d18eb2f443c0
Gautham Nair <gautham.p.nair@gmail.com>
2014-09-26 11:47:19 -0400

    up to the implementation of Either. ex4.6

--- fpinCh04.scala
+++ fpinCh04.scala
@@ -121,7 +121,7 @@ object ex43{
 ex43.test
 
 
-object ex44{
+object ex44 {
 	import ex43.map2
 	def sequence[T](xos: List[Option[T]]): Option[List[T]] = 
 		xos match {
@@ -136,3 +136,116 @@ object ex44{
 	}
 }
 ex44.test
+
+
+def Try[T](t: => T): Option[T] =
+	try {Some(t)} catch { case e: Exception => None } 
+
+def testTry {
+	assert(Try("a".toInt) == None)
+	assert(Try("123".toInt) == Some(123))
+}
+testTry
+
+
+
+
+object ex45 {
+	def parseIntsWithSeq(as: List[String]): Option[List[Int]] =
+		ex44.sequence(as.map(a => Try(a.toInt)))
+
+	import ex43.map2
+	def traverse[T, R](xs: List[T])(f: T => Option[R]): Option[List[R]] = {
+		xs match {
+			case x :: xrest => map2(f(x), traverse(xrest)(f))(_ :: _)
+			case Nil => Some(Nil)
+		}
+	}
+	def parseInts(as: List[String]): Option[List[Int]] =
+		traverse(as)(a => Try(a.toInt))
+
+	def sequence[T](xs: List[Option[T]]): Option[List[T]] =
+		traverse(xs)(x => x)
+
+	def testParse {
+		assert(parseIntsWithSeq(List("1","2")) == Some(List(1,2)))
+		assert(parseIntsWithSeq(List("1","b")) == None)
+		assert(parseInts(List("1","2")) == Some(List(1,2)))
+		assert(parseInts(List("1","b")) == None)
+	}
+
+	def test {
+		testParse
+		assert(sequence(Some(2) :: Some(1) :: Nil) == Some(2 :: 1 :: Nil))
+		assert(sequence(Some(2) :: None :: Nil) == None)
+		assert(sequence(Nil) == Some(Nil))
+	}
+}
+ex45.test
+
+
+// this is a *disjoint* union of two possibilities.
+// The "Right" value is the desirable output
+// the Left is the "error" outcome
+
+sealed trait Either[+E, +R] {
+	def map[R2](f: R => R2): Either[E, R2]
+	// if we are already fail we'll be returning an Either[E, R2]
+	// which we must be able to consider as an Either[E2, R2]
+	// so we need E <: E2
+	def flatMap[E2 >: E, R2](f: R => Either[E2, R2]): Either[E2, R2]
+	// we need Either[E, R] <: Either[E2, R2], which requires E <: E2, R <: R2
+	def orElse[E2 >: E, R2 >: R](alternative: => Either[E2, R2]): Either[E2, R2]
+
+	// when you request a.map2(b), the compiler has
+	// the freedom to choose E3. This means that b could come in with a type
+	// E2 originally. as long as it can be cast into an E3  and that E3 also
+	// be a parent of a, that is the version of map that will be used.
+	def map2[E3 >: E, R2, R3](b: Either[E3, R2])
+							(f: (R, R2) => R3): Either[E3, R3]
+}
+case class Left[+E](value: E) extends Either[E, Nothing] {
+	def map[R2](f: Nothing => R2): Either[E, Nothing] = this
+	def flatMap[E2 >: E, R2](f: Nothing => Either[E2, R2]): Either[E2, R2] = Left(value)
+	def orElse[E2 >: E, R2](alternative: => Either[E2, R2]): Either[E2, R2] = 
+		alternative
+	def map2[E3 >: E, R2, R3](b: Either[E3, R2])
+					(f: (Nothing, R2) => R3): Either[E3, R3] = this
+}
+case class Right[+R](value: R) extends Either[Nothing, R] {
+	def map[R2](f: R => R2): Either[Nothing, R2] = Right(f(value))
+	def flatMap[E2, R2](f: R => Either[E2, R2]): Either[E2, R2] = f(value)
+	def orElse[E2, R2 >: R](alternative: => Either[E2, R2]): Either[E2, R2] =
+		this
+	def map2[E3, R2, R3](b: Either[E3, R2])
+						(f: (R, R2) => R3): Either[E3, R3] = b.map(f(value, _))
+}
+
+object ex46 {
+	def safeInv(x: Int): Either[String, Int] = {
+		if (x == 0) Left("divide by zero")
+		else Right(1/x)
+	}
+	def test {
+		val intValue: Either[String, Int] = Right(3)
+		val intError: Either[String, Int] = Left("error")
+
+		assert(intValue.map(2 * _) == Right(6))
+		assert(intError.map(2 * _) == Left("error"))
+
+		val zeroValue: Either[String, Int] = Right(0)
+
+		assert(intValue.flatMap(safeInv(_)) == Right(0))
+		assert(intError.flatMap(safeInv(_)) == Left("error"))
+		assert(zeroValue.flatMap(safeInv(_)) == Left("divide by zero"))
+
+		assert(intError.orElse(intValue) == intValue)
+		assert(intValue.orElse(intError) == intValue)
+		assert(intError.orElse(zeroValue.flatMap(safeInv(_))) == Left("divide by zero"))
+
+		assert((intValue map2 intValue)(_ + _) == Right(6))
+		assert((intError map2 intValue)(_ + _) == Left("error"))
+		assert((intValue map2 intError)(_ + _) == Left("error"))
+	}
+}
+ex46.test
